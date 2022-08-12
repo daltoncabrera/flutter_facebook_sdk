530256906884e09100a1bc2e94d9622a2ca83e47
beelze <66875799+beelzeui@users.noreply.github.com>
2022-08-12 08:29:15 +0700

    update type parameter logPurchase

--- ios/Classes/SwiftFlutterFacebookSdkPlugin.swift
+++ ios/Classes/SwiftFlutterFacebookSdkPlugin.swift
@@ -103,7 +103,7 @@ public class SwiftFlutterFacebookSdkPlugin: NSObject, FlutterPlugin, FlutterStre
         AppEvents.shared.logEvent(.completedRegistration, parameters: parameters)
     }
     
-    func logPurchase(amount:Double, currency:String, parameters: [AppEvents.name: Any]){
+    func logPurchase(amount:Double, currency:String, parameters: Dictionary<AppEvents.ParameterName, Any>){
         AppEvents.shared.logPurchase(amount: amount, currency: currency, parameters: parameters)
     }
     
